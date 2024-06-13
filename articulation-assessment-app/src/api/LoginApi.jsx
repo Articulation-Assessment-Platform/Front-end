257299777cd84fe9591eeddacba926e2bdc03f43
MaikeMee <113889971+MaikeMee@users.noreply.github.com>
2024-06-13 14:26:52 +0200

    Update LoginApi.jsx

--- articulation-assessment-app/src/api/LoginApi.jsx
+++ articulation-assessment-app/src/api/LoginApi.jsx
@@ -1,4 +1,4 @@
-const apiUrl = '${process.env.API_GATEWAY_URL}/auth-api/api/auth/login';
+const apiUrl = 'http://10.0.61.102:5000/auth-api/api/auth/login';
 
 const LoginApi = async (loginCredentials) => {
     const requestOptions = {
