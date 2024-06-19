68d6913e8e7e0db06ae336efed40c57d4b1378c4
Meek,Maike M.Z.E <m.meek@student.fontys.nl>
2024-06-19 18:17:52 +0200

    add

--- articulation-assessment-app/src/api/AzureKeyVault.jsx
+++ articulation-assessment-app/src/api/AzureKeyVault.jsx
@@ -8,9 +8,8 @@ export const getApiUrl = async () => {
     // Use InteractiveBrowserCredential for browser-based authentication
     const credential = new InteractiveBrowserCredential({
         clientId: "c1e00095-7b2c-4808-a14a-23cd6f91f614  ",  
-        tenantId: "c66b6765-b794-4a2b-84ed-845b341c086a",
-        clientSecret: "lxg8Q~VJh6PI9gPN7ccyQjh8ZfDkIvNbSIPWAcs5"  
-    });
+        tenantId: "c66b6765-b794-4a2b-84ed-845b341c086a"
+     });
     
     const client = new SecretClient(kvUri, credential);
 
